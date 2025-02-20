4cffbe5dd93f03acf7e80cb2fc736f1e21134955
Bui Ngoc Tuyen <bntuyena2.2019c3locthanh@gmail.com>
2025-02-20 11:15:09 +0700

    cấu hình routes

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,5 @@ use App\Http\Controllers\API\ProductController;
 
 Route::get('/v1/products', [ProductController::class, 'index']);
 Route::post('/v1/products', [ProductController::class, 'store']);
+
+// cbxc
